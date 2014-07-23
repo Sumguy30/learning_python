e5225f750317d37bc0c25d7c4241b5744888bf31
Sumguy30 <Sumguy30@users.noreply.github.com>
2014-07-23 17:28:26 -0400

    added homework class

--- OfficeInventory.py
+++ OfficeInventory.py
@@ -5,7 +5,7 @@ class OfficeInventory:
     def __init__(self, items={}):
         if type(items) != type({}):
             raise TypeError("Wrong Type Asshole, use a list stupid!")
-        self.passeditems=items
+        self.passeditems = items
         return
 
 
@@ -54,11 +54,41 @@ class OfficeInventory:
     def __show_inv(self,item, quantity, addflag):
         if addflag == "addinv":
                   print("%d of %s was added to Office Inventory\n" % (quantity, item))
-                  print("Here is new Inventory: %s" % (self.passeditems))
+                  self.show_inventory()
         else:
                   print("%d of %s was removed from Office Inventory" % (quantity, item))
-                  print("Here is new Inventory: %s" % (self.passeditems))
+                  self.show_inventory()
 
     def show_inventory(self):
         print("Here is the current Inventory")
-        print("%s" % (self.passeditems))
+        for items in self.passeditems.keys():
+                print("Item: %s   Quantity: %d" % (items, self.passeditems[items]))
+
+
+
+
+class HomeWork:
+    """ Creates Homework"""
+    def __init__(self,subject="math"):
+        self.__set_subject(subject)
+        return
+
+    def __set_subject(self, subject):
+        if subject == "math":
+            return {"paper":2,"pen":1}
+        elif subject == "online gaming":
+            return {"computer":1,"mouse":1}
+        elif subject == "art":
+            return {"paper":1,"ruler":1}
+        else:
+            return False
+
+    def get_inventory_from_office(self,OffInvObj,subject):
+        subjectrequirements = self.__set_subject(subject)
+        for item in subjectrequirements.keys():
+            OffInvObj.remove_inventory(item,subjectrequirements[item])
+        OffInvObj.show_inventory
+
+
+
+    
